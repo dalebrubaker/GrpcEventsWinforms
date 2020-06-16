8560395d2a6dad588f8e85f6c9c70696c5f092e4
Dale Brubaker <dale@ebrubaker.com>
2020-06-16 10:07:46 -0400

    Add Subscription class for client

--- Client/FormClient.cs
+++ Client/FormClient.cs
@@ -1,48 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Linq;
 using System.Windows.Forms;
-using Common;
-using Grpc.Core;
-using GrpcEvents;
 
 namespace Client
 {
     public partial class FormClient : Form
     {
-        private readonly EventsService.EventsServiceClient _client;
-        private AsyncDuplexStreamingCall<SubscribeRequest, SampleEventArgsMessage> _call;
         private static int _sClientNumber;
         private int _clientNumber;
-        private readonly List<string> _accountNamesSubscribed = new List<string>();
+        private readonly List<Subscription> _subscriptions = new List<Subscription>();
 
         public FormClient()
         {
             InitializeComponent();
-            var channel = new Channel("localhost:" + Constants.ServerPort, ChannelCredentials.Insecure);
-            _client = new EventsService.EventsServiceClient(channel);
-            Disposed += OnDisposed;
-        }
-
-        private void OnDisposed(object sender, EventArgs e)
-        {
-            _call.Dispose();
-            _call = null;
         }
 
         private void btnStartClient_Click(object sender, EventArgs e)
         {
         }
 
-        private async void btnStopClient_Click(object sender, EventArgs e)
+        private void btnStopClient_Click(object sender, EventArgs e)
         {
-            var request = new SubscribeRequest
-            {
-                StartStop = false
-            };
-            logControl1.LogMessage($"{Text} is unsubscribing to event {request.AccountName}");
-            await _call.RequestStream.WriteAsync(request);
         }
 
         private void FormClient_Load(object sender, EventArgs e)
@@ -59,63 +38,17 @@ namespace Client
                 MessageBox.Show(this, "Enter an account name to which to subscribe events.");
                 return;
             }
-            if (_accountNamesSubscribed.Contains(accountName))
+            if (_subscriptions.Exists(x => x.AccountName == accountName))
             {
                 MessageBox.Show(this, "Duplicate account names are not allowed.");
                 return;
             }
-            if (_call == null)
-            {
-                _ = Task.Run(StartReceivingEvents);
-            }
-            _accountNamesSubscribed.Add(accountName);
-            var request = new SubscribeRequest
-            {
-                StartStop = true,
-                AccountName = accountName
-            };
-            logControl1.LogMessage($"{Text} is subscribing to event {request.AccountName}");
-            try
-            {
-                await _call.RequestStream.WriteAsync(request);
-            }
-            catch (RpcException ex)
-            {
-                logControl1.LogMessage(ex.Message);
-                _call = null;
-                throw;
-            }
+            var subscription = new Subscription(accountName, Text, logControl1);
+            _subscriptions.Add(subscription);
+            await subscription.SubscribeAsync();
         }
 
-        private async Task StartReceivingEvents()
-        {
-            try
-            {
-                using (_call = _client.Subscribe())
-                {
-                    logControl1.LogMessage($"{Text} is waiting for events...");
-                    while (await _call.ResponseStream.MoveNext(CancellationToken.None))
-                    {
-                        var sampleEventArgsMessage = _call.ResponseStream.Current;
-                        var sampleEventArgs = sampleEventArgsMessage.ToSampleEventArgs();
-                        var msg = $"Received {sampleEventArgs}";
-                        logControl1.LogMessage(msg);
-                        var account = new Account(sampleEventArgs.AccountName);
-                        account.OnSampleEvent(sampleEventArgs);
-                        msg = $"Re-threw event client-side {sampleEventArgs}";
-                        logControl1.LogMessage(msg);
-                    }
-                }
-            }
-            catch (RpcException ex)
-            {
-                logControl1.LogMessage(ex.Message);
-                _call = null;
-                throw;
-            }
-        }
-
-        private async void btnUnsubscribeEvent_Click(object sender, EventArgs e)
+        public async void btnUnsubscribeEvent_Click(object sender, EventArgs e)
         {
             var accountName = txtAccountName.Text;
             if (string.IsNullOrEmpty(accountName))
@@ -123,28 +56,14 @@ namespace Client
                 MessageBox.Show(this, "Enter an account name from which to unsubscribe events.");
                 return;
             }
-            if (!_accountNamesSubscribed.Contains(accountName))
+            var subscription = _subscriptions.FirstOrDefault(x => x.AccountName == accountName);
+            if (subscription == null)
             {
                 MessageBox.Show(this, $"Not currently subscribed to {accountName}");
                 return;
             }
-            _accountNamesSubscribed.Remove(accountName);
-            var request = new SubscribeRequest
-            {
-                StartStop = true,
-                AccountName = txtAccountName.Text
-            };
-            try
-            {
-                logControl1.LogMessage($"{Text} is un-subscribing from event {request.AccountName}");
-                await _call.RequestStream.WriteAsync(request);
-            }
-            catch (RpcException ex)
-            {
-                logControl1.LogMessage(ex.Message);
-                _call = null;
-                throw;
-            }
+            _subscriptions.Remove(subscription);
+            await subscription.UnsubscribeAsync();
         }
     }
 }
--- Client/Subscription.cs
+++ Client/Subscription.cs
@@ -1,17 +1,111 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Common;
+using Grpc.Core;
+using GrpcEvents;
+
 namespace Client
 {
-    public class Subscription
+    public class Subscription : IDisposable
     {
+        private readonly string _clientName;
+        private readonly LogControl _logControl;
+        private readonly EventsService.EventsServiceClient _client;
+        private AsyncDuplexStreamingCall<SubscribeRequest, SampleEventArgsMessage> _call;
+
         public string AccountName { get; }
 
-        public Subscription(string accountName)
+        public Subscription(string accountName, string clientName, LogControl logControl)
         {
+            _clientName = clientName;
+            _logControl = logControl;
             AccountName = accountName;
+            var channel = new Channel("localhost:" + Constants.ServerPort, ChannelCredentials.Insecure);
+            _client = new EventsService.EventsServiceClient(channel);
+        }
+
+        public async Task SubscribeAsync()
+        {
+            if (_call == null)
+            {
+                _ = Task.Run(StartReceivingEvents);
+            }
+            var request = new SubscribeRequest
+            {
+                StartStop = true,
+                AccountName = AccountName
+            };
+            _logControl.LogMessage($"{_clientName} is subscribing to event {request.AccountName}");
+            try
+            {
+                await _call.RequestStream.WriteAsync(request);
+            }
+            catch (RpcException ex)
+            {
+                _logControl.LogMessage(ex.Message);
+                _call = null;
+                throw;
+            }
+        }
+
+        private async Task StartReceivingEvents()
+        {
+            try
+            {
+                using (_call = _client.Subscribe())
+                {
+                    _logControl.LogMessage($"{_clientName} is waiting for events...");
+                    while (await _call.ResponseStream.MoveNext(CancellationToken.None))
+                    {
+                        var sampleEventArgsMessage = _call.ResponseStream.Current;
+                        var sampleEventArgs = sampleEventArgsMessage.ToSampleEventArgs();
+                        var msg = $"Received {sampleEventArgs}";
+                        _logControl.LogMessage(msg);
+                        var account = new Account(sampleEventArgs.AccountName);
+                        account.OnSampleEvent(sampleEventArgs);
+                        msg = $"Re-threw event client-side {sampleEventArgs}";
+                        _logControl.LogMessage(msg);
+                    }
+                }
+            }
+            catch (RpcException ex)
+            {
+                _logControl.LogMessage(ex.Message);
+                _call = null;
+                throw;
+            }
+        }
+
+        public async Task UnsubscribeAsync()
+        {
+            var request = new SubscribeRequest
+            {
+                StartStop = true,
+                AccountName = AccountName
+            };
+            try
+            {
+                _logControl.LogMessage($"{_clientName} is un-subscribing from event {request.AccountName}");
+                await _call.RequestStream.WriteAsync(request);
+            }
+            catch (RpcException ex)
+            {
+                _logControl.LogMessage(ex.Message);
+                _call = null;
+                throw;
+            }
         }
 
         public override string ToString()
         {
             return AccountName;
+        }
+
+        public void Dispose()
+        {
+            _call?.Dispose();
+            _call = null;
         }
     }
 }
