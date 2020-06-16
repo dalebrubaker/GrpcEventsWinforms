a3326912581a41d3b54a1942907dbca09753c3b2
Dale Brubaker <dale@ebrubaker.com>
2020-06-16 06:15:11 -0400

    Start forms

--- Client/FormClient.Designer.cs
+++ Client/FormClient.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Client
 {
-    partial class Form1
+    partial class FormClient
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormClient";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
--- Client/FormClient.cs
+++ Client/FormClient.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Client
 {
-    public partial class Form1 : Form
+    public partial class FormClient : Form
     {
-        public Form1()
+        public FormClient()
         {
             InitializeComponent();
         }
